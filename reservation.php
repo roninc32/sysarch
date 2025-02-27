de3c88a375fc408b165f18ca6a63982791733d9d
unknown <TT411@ccs.local>
2025-02-27 11:57:16 +0800

    initial update

--- reservation.php
+++ reservation.php
@@ -30,6 +30,7 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reservation</title>
+    <link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-100 min-h-screen flex flex-col">
